2a472a57556ab211c3ff75534a50dfa7a7fb59d7
Jake Halpern <jmh2363@columbia.edu>
2026-04-11 08:36:08 -0700

    A variety of batch scripts

--- examples/dipoles/batch_scan.sh
+++ examples/dipoles/batch_scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -A m1266
+#SBATCH -A m4680
 #SBATCH --job-name=single_stage
-#SBATCH --time=24:00:00
+#SBATCH --time=30:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=128   # one process per core; each core runs one optimization
@@ -9,14 +9,8 @@
 #SBATCH --qos=regular
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jacobhalpern667@gmail.com
-
-# Redirect stdout/stderr to a dated, run-count, job-specific log file:
-#   YYMMDD_<N>_<JOBID>.out
-OUT_DIR="../slurm_outputs"
-mkdir -p "$OUT_DIR"
-DATE_STR=$(date +%y%m%d)
-OUT_FILE="${OUT_DIR}/${DATE_STR}_${SLURM_JOB_ID:-local}.out"
-exec >"$OUT_FILE" 2>&1
+#SBATCH --output=../slurm_outputs/%x_%j.out
+#SBATCH --error=../slurm_outputs/%x_%j.out
 
 # Purge all modules to remove the "Application linked against multiple cray-libsci libraries" warning
 module purge
@@ -35,6 +29,10 @@ N=$(python3 generate_inputs.py --total)
 # Stage 2 directory to initialize from
 #INIT_DIR="../outputs/stage_2_npol11_ntor8_wout_nfp22ginsburg_000_000281/91_npol_11_ntor_8_VV_a_0.229_VV_b_0.265_VV_R0_1.038"
 INIT_DIR="../outputs/stage_2_npol10_ntor8_wout_nfp22ginsburg_000_000281/90_npol_10_ntor_8_VV_a_0.250_VV_b_0.282_VV_R0_1.042"
+#INIT_DIR="../outputs/stage_2_npol10_ntor8_wout_nfp22ginsburg_000_001242/08_npol_10_ntor_8_VV_a_0.245_VV_b_0.297_VV_R0_1.015"
+#INIT_DIR="../outputs/stage_2_npol11_ntor8_wout_nfp22ginsburg_000_001242/01_npol_11_ntor_8_VV_a_0.235_VV_b_0.268_VV_R0_1.022"
+
+VOL_TARGET=0.40
 
 # How many runs to execute in parallel on this node and how many threads to use for each.
 export OMP_NUM_THREADS=8        # or 8, 16 – you can experiment
@@ -50,7 +48,7 @@ for i in $(seq 0 $((N-1))); do
     echo "[$((i+1))/$N] Starting optimization $i with parameters $PARAMS"
 
     # Launch this optimization in the background.
-    python3 single_stage_dipole_example.py --init-dir "$INIT_DIR" $PARAMS &
+    python3 single_stage_epsilon_constraint.py --init-dir "$INIT_DIR" --vol-target $VOL_TARGET $PARAMS &
     ((running++))
 
     # If we've reached the concurrency limit, wait for at least one job to finish.
